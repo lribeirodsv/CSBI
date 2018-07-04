108a4853fe9461fcc28a5b090d18dddc77109930
Lucas Ribeiro <lribeiro.dsv@gmail.com>
2018-07-03 23:00:10 -0300

    Project creation

---
+++ src/main/java/com/lribeiro/csbi/CsbiApplication.java
@@ -0,0 +1,12 @@
+package com.lribeiro.csbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsbiApplication.class, args);
+	}
+}
